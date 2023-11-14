5b31fdd54c94d20194347766ed161577eefa0cf7
Ahmedsamy270 <ahmedsamy270@gmail.com>
2023-11-14 12:29:18 +0200

    _strlen

--- _strlen.c
+++ _strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strlen - is a function that counts the length of a string
- * @n: is a pointer to yhe begining of the string
- * Return: Number Of Counted Charecters
+ * @n: is a pointer to the begining of the string
+ * Return: Number Of Counted Printed Charecters
  */
 
 int _strlen(const char *n)
